a71999b3f0162e94412ef638e8d169aff1dc1749
TikChun <tikchuntong@163.com>
2023-05-05 15:47:01 -0400

    metapackage

--- Chapter2_communication/demo01/build/cmake_install.cmake
+++ Chapter2_communication/demo01/build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/tikchuntong/Git_ROS/ROS_Ubuntu/Chapter2_communication/demo01/build/gtest/cmake_install.cmake")
+  include("/home/tikchuntong/Git_ROS/ROS_Ubuntu/Chapter2_communication/demo01/build/plumbing_my/cmake_install.cmake")
   include("/home/tikchuntong/Git_ROS/ROS_Ubuntu/Chapter2_communication/demo01/build/plumbing_head/cmake_install.cmake")
   include("/home/tikchuntong/Git_ROS/ROS_Ubuntu/Chapter2_communication/demo01/build/plumbing_head_src/cmake_install.cmake")
   include("/home/tikchuntong/Git_ROS/ROS_Ubuntu/Chapter2_communication/demo01/build/plumbing_param_server/cmake_install.cmake")
---
+++ Chapter2_communication/demo01/build/plumbing_my/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tikchuntong/Git_ROS/ROS_Ubuntu/Chapter2_communication/demo01/src/plumbing_my
+# Build directory: /home/tikchuntong/Git_ROS/ROS_Ubuntu/Chapter2_communication/demo01/build/plumbing_my
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
