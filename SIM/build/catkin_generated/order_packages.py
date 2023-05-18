e7c251698166feef39ec89644050d1fff9c10f9b
TikChun <tikchuntong@163.com>
2023-05-18 05:29:24 -0400

    start urdf

---
+++ SIM/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/tikchuntong/Git_ROS/ROS_Ubuntu/SIM/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/tikchuntong/ROS_education/demo01/devel;/opt/ros/noetic'.split(';') if '/home/tikchuntong/ROS_education/demo01/devel;/opt/ros/noetic' != '' else []
---
+++ SIM/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/tikchuntong/Git_ROS/ROS_Ubuntu/SIM/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/tikchuntong/Git_ROS/ROS_Ubuntu/SIM/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/tikchuntong/Git_ROS/ROS_Ubuntu/SIM/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/tikchuntong/Git_ROS/ROS_Ubuntu/SIM/src:$ROS_PACKAGE_PATH"
